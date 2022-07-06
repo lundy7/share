128fc79b4d015f0e9cde1bd0df0619cec7e03321
Lundy <lundy@example.com>
2022-07-06 21:01:58 +0700

    SignIn and LogIn design commit.

---
+++ app/src/main/java/com/lundy/share/Profile/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.lundy.share.Profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lundy.share.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
